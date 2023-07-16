3fa2044a902f3f58959d6fbdc6e6583893cc960b
ArhitaK <113281167+ArhitaK@users.noreply.github.com>
2023-07-17 02:25:45 +0530

    Add files via upload

---
+++ client/src/components/ChannelSearch.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import { useChatContext } from 'stream-chat-react';
+
+import { SearchIcon } from '../assets';
+
+const ChannelSearch = () =>{
+  const [query, setQuery] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const getChannels = async (text) =>{
+    try{
+      //1000: fetch channels
+    } catch(error){
+      setQuery()
+    }
+  }
+
+  const onSearch = (event) =>{
+    event.preventDefault();
+
+    setLoading(true);
+    setQuery(event.target.value);
+    getChannels(event.target.value);
+  }
+
+  
+  return(
+    <div className="channel-search__container">
+      <div className="channel-search__input__wrapper">
+        <div className="channel-search__input__icon">
+          <SearchIcon />
+          <div></div>
+          <input 
+            className="channel-search__input_-text"
+            placeholder="Search"
+            type="text"
+            value={query} 
+            onChange={onSearch}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ChannelSearch
+
